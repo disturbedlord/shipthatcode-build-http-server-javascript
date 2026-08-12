dd7c44cfee7b766213fbc6b9c09323a9b71b8a00
Avinash K <todisturbedlord@gmail.com>
2026-08-13 01:14:44 +0530

    Build HTTP Response

--- dist/main.js
+++ dist/main.js
@@ -1,18 +1,82 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const STATUS_TEXT = {
+    200: "OK",
+    201: "Created",
+    204: "No Content",
+    301: "Moved Permanently",
+    302: "Found",
+    304: "Not Modified",
+    400: "Bad Request",
+    401: "Unauthorized",
+    403: "Forbidden",
+    404: "Not Found",
+    405: "Method Not Allowed",
+    500: "Internal Server Error",
+};
+class Response {
+    statusCode = 200;
+    headerCount = 0;
+    headers = new Map();
+    body = "";
+    formatStatusLine = () => `HTTP/1.1 ${this.statusCode} ${STATUS_TEXT[this.statusCode]}`;
+    formatHeader = (key, val) => `${key} ${val}`;
+    appendHeaders = () => {
+        let requestHeaders = [];
+        this.headers.forEach((v, k) => {
+            requestHeaders.push(this.formatHeader(k, v));
+        });
+        return requestHeaders.join("\r\n");
+    };
+    appendDelimiter = () => "\r\n";
+    appendBody = () => (this.body != "" ? this.body : "");
+    ToString = () => {
+        console.log(`${this.formatStatusLine()}${this.appendDelimiter()}${this.appendHeaders()}${this.appendDelimiter()}${this.appendDelimiter()}${this.appendBody()}`);
+    };
+}
+const STATES = {
+    1: "StatusCode",
+    2: "headers",
+    3: "body",
+};
 const data = require("fs").readFileSync(0, "utf8").split("\n");
 let i = 0;
-let content = "";
+let currentState = 1; // Parse Status Line
+const res = new Response();
 while (i < data.length) {
-    let recvContentLength = data[i].trim();
-    let contentLength = parseInt(recvContentLength, 16);
-    if (contentLength === 0)
-        break;
-    if (i + 1 < data.length) {
-        let contentToParse = data[i + 1].substring(0, contentLength);
-        content += contentToParse;
+    switch (currentState) {
+        case 1: {
+            let [code, headerCount] = data[i]?.split(" ");
+            res.statusCode = Number(code);
+            res.headerCount = +headerCount;
+            currentState++; // Parse Headers
+            if (+headerCount === 0)
+                currentState = 3;
+            break;
+        }
+        case 2: {
+            //console.log(res.headerCount);
+            if (res.headerCount > 0) {
+                let header = data[i]?.split(" ");
+                let key = header[0];
+                let value = header.slice(1).join(" ");
+                key = key?.trim();
+                value = value?.trim();
+                if (key && value)
+                    res.headers.set(key, value);
+                res.headerCount--;
+            }
+            if (res.headerCount === 0) {
+                currentState++; // Parse Body
+            }
+            break;
+        }
+        case 3: {
+            res.body += data[i];
+        }
     }
-    i += 2;
+    i++;
 }
-console.log(content);
+res.headers.set("Content-Length:", res.body.length.toString());
+res.ToString();
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,17 +1,100 @@
-const data = require("fs").readFileSync(0, "utf8").split("\n");
-let i = 0;
-let content = "";
-while (i < data.length) {
-  let recvContentLength = data[i].trim();
-  let contentLength = parseInt(recvContentLength, 16);
+type STATUS_CODES =
+  | 200
+  | 201
+  | 204
+  | 301
+  | 302
+  | 304
+  | 400
+  | 401
+  | 403
+  | 404
+  | 405
+  | 500;
 
-  if (contentLength === 0) break;
+const STATUS_TEXT = {
+  200: "OK",
+  201: "Created",
+  204: "No Content",
+  301: "Moved Permanently",
+  302: "Found",
+  304: "Not Modified",
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
+  405: "Method Not Allowed",
+  500: "Internal Server Error",
+};
 
-  if (i + 1 < data.length) {
-    let contentToParse = data[i + 1].substring(0, contentLength);
-    content += contentToParse;
-  }
-  i += 2;
+class Response {
+  statusCode: STATUS_CODES = 200;
+  headerCount = 0;
+  headers: Map<string, string> = new Map();
+  body: string = "";
+
+  formatStatusLine = () =>
+    `HTTP/1.1 ${this.statusCode} ${STATUS_TEXT[this.statusCode]}`;
+  formatHeader = (key: string, val: string) => `${key} ${val}`;
+  appendHeaders = () => {
+    let requestHeaders: string[] = [];
+    this.headers.forEach((v: string, k: string) => {
+      requestHeaders.push(this.formatHeader(k, v));
+    });
+    return requestHeaders.join("\r\n");
+  };
+
+  appendDelimiter = () => "\r\n";
+  appendBody = () => (this.body != "" ? this.body : "");
+  ToString = () => {
+    console.log(
+      `${this.formatStatusLine()}${this.appendDelimiter()}${this.appendHeaders()}${this.appendDelimiter()}${this.appendDelimiter()}${this.appendBody()}`,
+    );
+  };
 }
 
-console.log(content);
+const STATES = {
+  1: "StatusCode",
+  2: "headers",
+  3: "body",
+};
+
+const data: string[] = require("fs").readFileSync(0, "utf8").split("\n");
+let i = 0;
+let currentState = 1; // Parse Status Line
+const res = new Response();
+while (i < data.length) {
+  switch (currentState) {
+    case 1: {
+      let [code, headerCount] = data[i]?.split(" ")!;
+      res.statusCode = Number(code) as STATUS_CODES;
+      res.headerCount = +headerCount!;
+
+      currentState++; // Parse Headers
+      if (+headerCount! === 0) currentState = 3;
+      break;
+    }
+    case 2: {
+      //console.log(res.headerCount);
+      if (res.headerCount > 0) {
+        let header = data[i]?.split(" ")!;
+        let key = header[0];
+        let value = header.slice(1).join(" ");
+        key = key?.trim();
+        value = value?.trim();
+        if (key && value) res.headers.set(key, value);
+        res.headerCount--;
+      }
+      if (res.headerCount === 0) {
+        currentState++; // Parse Body
+      }
+      break;
+    }
+    case 3: {
+      res.body += data[i]!;
+    }
+  }
+  i++;
+}
+res.headers.set("Content-Length:", res.body.length.toString());
+res.ToString();
