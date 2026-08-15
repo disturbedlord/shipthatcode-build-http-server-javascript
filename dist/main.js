2785925b9e468bb4050e578513ace401f9a608ec
Avinash K <todisturbedlord@gmail.com>
2026-08-15 14:09:09 +0530

    done

--- dist/main.js
+++ dist/main.js
@@ -1,152 +1,158 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const data = require("fs").readFileSync(0, "utf8").split("\n");
-let i = 0;
-class Trie {
-    children = new Array(128).fill(undefined);
-    isParam = false;
+class Tree {
+    type = "";
     isEnd = false;
-    path = "";
-    param = "";
-    verb = "";
-    action = "";
-}
-const isSlash = (x) => {
-    if (x === "/")
-        return true;
-    else
-        return false;
-};
-const root = new Trie();
-const AddPathToTrie = (p, a, v) => {
-    let head = root;
-    for (let i = 0; i < p.length; i++) {
-        if (p[i] === "{") {
-            let param = "";
-            while (p[i] !== "}") {
-                if (p[i] != "{" && p[i] != "}") {
-                    param += p[i];
-                }
-                i++;
-            }
-            head.param = param; // {id} / {post}
-            head.isParam = true;
-        }
-        else {
-            //if not params or query
-            const idx = p[i].charCodeAt(0);
-            if (head.children[idx] === undefined)
-                head.children[idx] = new Trie();
-            head = head.children[idx];
-        }
+    value = "";
+    children = [];
+    constructor(v) {
+        this.value = v;
     }
-    head.isEnd = true;
-    head.verb = v;
-    head.action = a;
-};
-const HandleRequest = (p, v) => {
-    let head = root;
+    route = [];
+}
+let i = 0;
+let mode = 0;
+const parseRequest = (s) => {
+    let resources = [];
     let i = 0;
-    let action = "";
-    let attr = [];
-    const getIdx = (x) => x.charCodeAt(0);
-    while (i < p.length && head != undefined) {
-        let idx = getIdx(p[i]);
-        if (!head.isParam && !head.children[idx] && p[i] !== "?")
+    let resource = "";
+    while (i < s.length) {
+        if (s[i] === "?") {
+            if (resource != "") {
+                resources.push(resource);
+                resource = "";
+            }
+            resources.push(s.substring(i));
             break;
-        //console.log(idx);
-        if (head.children[idx])
-            head = head.children[idx];
+        }
+        if (s[i] === "/") {
+            if (resource != "")
+                resources.push(resource);
+            resource = "";
+        }
         else {
-            //console.log(p[i]);
-            if (head.isParam) {
-                let paramVal = "";
-                while (i < p.length && p[i] !== "/") {
-                    paramVal += p[i];
-                    i++;
-                }
-                attr.push(`${head.param}=${paramVal}`);
-                if (i < p.length && p[i] === "/")
-                    continue;
-            }
-            else if (head.isEnd) {
-                //Handle Query String
-                if (p[i] === "?") {
-                    i++;
-                    let q = "";
-                    while (i < p.length) {
-                        if (p[i] === "&") {
-                            attr.push(`${q}`);
-                            q = "";
-                        }
-                        else {
-                            q += p[i];
-                        }
-                        i++;
-                    }
-                    if (q !== "")
-                        attr.push(q);
-                }
-            }
+            resource += s[i];
         }
         i++;
     }
-    if (head && head.isEnd) {
-        if (head.isParam && attr.length === 0) {
-            console.log("404");
+    if (resource !== "")
+        resources.push(resource);
+    return resources;
+};
+const TreeHelper = {
+    searchByValue: (node, search) => {
+        let i = 0;
+        while (i < node.children.length) {
+            const curr = node.children[i];
+            if (curr?.value === search)
+                return curr;
+            i++;
         }
+        return null;
+    },
+};
+const root = new Tree("root");
+const addToTree = (components, v, a) => {
+    let head = root;
+    components.forEach((ele) => {
+        let node = TreeHelper.searchByValue(head, ele);
+        if (!node) {
+            // Create a new Node in Tree
+            node = new Tree(ele);
+            node.type = ele.startsWith("{") ? "param" : "static";
+            head.children.push(node);
+        }
+        head = node;
+    });
+    head.isEnd = true;
+    head.route?.push({
+        verb: v,
+        action: a,
+    });
+};
+const matchRoute = (components, verb) => {
+    //console.log("======================", components);
+    let head = root;
+    let i = 0;
+    let leafNode = undefined;
+    let paramNodes = [];
+    const traverseTree = (node, components, i) => {
+        if (i >= components.length || !node)
+            return;
+        if (components[i]?.startsWith("?"))
+            return node;
+        let foundNode = TreeHelper.searchByValue(node, components[i]);
+        //console.log("------------\n", foundNode);
+        if (!foundNode && node.children.length === 1) {
+            if (node.children[0]?.type === "param") {
+                paramNodes.push(`${node.children[0].value.substring(1, node.children[0].value.length - 1)}=${components[i]}`);
+            }
+            if (i < components.length - 1) {
+                foundNode = node.children[0];
+            }
+            else {
+                if (node.children[0]?.isEnd === true) {
+                    return node.children[0];
+                }
+            }
+        }
+        if (!foundNode)
+            return;
+        // if (foundNode && foundNode.isEnd && i === components.length - 1) {
+        //   return foundNode;
+        // }
+        return traverseTree(foundNode, components, i + 1);
+    };
+    leafNode = traverseTree(head, components, 0);
+    paramNodes.sort();
+    //console.log(leafNode);
+    if (leafNode) {
+        let action = "";
+        leafNode.route?.forEach((r) => {
+            if (r.verb == verb) {
+                action = r.action;
+            }
+        });
+        if (paramNodes.length > 0)
+            console.log(`${action} ${paramNodes.join(" ")}`);
         else {
-            attr.sort();
-            if (attr.length > 0)
-                console.log(`${head.action} ${attr.join(" ")}`);
-            else
-                console.log(head.action);
+            console.log(`${action}`);
         }
     }
     else {
         console.log("404");
     }
-    // console.log(attr);
 };
-let mode = 0;
 while (i < data.length) {
     if (data[i] === "") {
-        mode++;
+        mode = 1;
     }
     else {
         switch (mode) {
             case 0: {
                 const [verb, path, action] = data[i]?.split(" ");
-                AddPathToTrie(path, action, verb);
+                const pathComponents = parseRequest(path);
+                addToTree(pathComponents, verb, action);
+                //console.log(pathComponents);
                 break;
             }
             case 1: {
                 const [verb, path] = data[i]?.split(" ");
-                HandleRequest(path, verb);
+                const pathComponents = parseRequest(path);
+                //console.log(pathComponents);
+                matchRoute(pathComponents, verb);
                 break;
             }
         }
     }
     i++;
 }
-let d = root;
-const test = (d) => {
-    let f = 0;
-    for (let i = 0; i < 27; i++) {
-        if (d.children[i] !== undefined) {
-            console.log(String.fromCharCode(i + 97), d.children[i]);
-            test(d.children[i]);
-            f = 1;
-        }
-    }
-    if (f == 0)
-        return;
+const visualizeTree = (root) => {
+    root.children.forEach((child) => {
+        console.log(child.type, child.value, child.isEnd, child.children.length, child.route);
+        visualizeTree(child);
+    });
 };
-for (let i = 0; i < 27; i++) {
-    if (d.children[i] !== undefined) {
-        console.log(String.fromCharCode(i + 97));
-        test(d.children[i]);
-    }
-}
-// console.log(root);
+//visualizeTree(root);
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,144 +1,172 @@
 const data: string[] = require("fs").readFileSync(0, "utf8").split("\n");
-let i = 0;
 
-class Trie {
-  children: Array<Trie> = new Array(128).fill(undefined);
-  isParam: boolean = false;
+class Tree {
+  type: "static" | "param" | "" = "";
   isEnd: boolean = false;
-  path: string = "";
-  param: string = "";
-  verb = "";
-  action = "";
+  value: string = "";
+  children: Tree[] = [];
+  constructor(v: string) {
+    this.value = v;
+  }
+  route?: {
+    verb: string;
+    action: string;
+  }[] = [];
 }
 
-const isSlash = (x: string) => {
-  if (x === "/") return true;
-  else return false;
-};
+let i = 0;
+let mode = 0;
 
-const root = new Trie();
-const AddPathToTrie = (p: string, a: string, v: string) => {
-  let head = root;
-  for (let i = 0; i < p.length; i++) {
-    if (p[i] === "{") {
-      let param = "";
-      while (p[i] !== "}") {
-        if (p[i] != "{" && p[i] != "}") {
-          param += p[i];
-        }
-        i++;
-      }
-      head.param = param; // {id} / {post}
-      head.isParam = true;
-    } else {
-      //if not params or query
-      const idx = p[i]!.charCodeAt(0);
-      if (head.children[idx] === undefined) head.children[idx] = new Trie();
-
-      head = head.children[idx];
-    }
-  }
-  head.isEnd = true;
-  head.verb = v;
-  head.action = a;
-};
-
-const HandleRequest = (p: string, v: string) => {
-  let head = root;
+const parseRequest = (s: string) => {
+  let resources = [] as string[];
   let i = 0;
-  let action = "";
-  let attr = [] as String[];
-  const getIdx = (x: string) => x.charCodeAt(0);
-  while (i < p.length && head != undefined) {
-    let idx = getIdx(p[i]!);
-    if (!head.isParam && !head.children[idx] && p[i] !== "?") break;
-    //console.log(idx);
-
-    if (head.children[idx]) head = head.children[idx]!;
-    else {
-      //console.log(p[i]);
-      if (head.isParam) {
-        let paramVal = "";
-        while (i < p.length && p[i] !== "/") {
-          paramVal += p[i];
-          i++;
-        }
-        attr.push(`${head.param}=${paramVal}`);
-        if (i < p.length && p[i] === "/") continue;
-      } else if (head.isEnd) {
-        //Handle Query String
-        if (p[i] === "?") {
-          i++;
-          let q = "";
-          while (i < p.length) {
-            if (p[i] === "&") {
-              attr.push(`${q}`);
-              q = "";
-            } else {
-              q += p[i];
-            }
-            i++;
-          }
-          if (q !== "") attr.push(q);
-        }
+  let resource = "";
+  while (i < s.length) {
+    if (s[i] === "?") {
+      if (resource != "") {
+        resources.push(resource);
+        resource = "";
       }
+      resources.push(s.substring(i));
+      break;
+    }
+    if (s[i] === "/") {
+      if (resource != "") resources.push(resource);
+      resource = "";
+    } else {
+      resource += s[i];
     }
     i++;
   }
-  if (head && head.isEnd) {
-    if (head.isParam && attr.length === 0) {
-      console.log("404");
-    } else {
-      attr.sort();
-      if (attr.length > 0) console.log(`${head.action} ${attr.join(" ")}`);
-      else console.log(head.action);
+  if (resource !== "") resources.push(resource);
+
+  return resources;
+};
+
+const TreeHelper = {
+  searchByValue: (node: Tree, search: string) => {
+    let i = 0;
+    while (i < node.children.length) {
+      const curr = node.children[i];
+      if (curr?.value === search) return curr;
+      i++;
+    }
+
+    return null;
+  },
+};
+
+const root = new Tree("root");
+
+const addToTree = (components: string[], v: string, a: string) => {
+  let head = root;
+  components.forEach((ele) => {
+    let node = TreeHelper.searchByValue(head, ele);
+    if (!node) {
+      // Create a new Node in Tree
+      node = new Tree(ele);
+      node.type = ele.startsWith("{") ? "param" : "static";
+      head.children.push(node);
+    }
+    head = node;
+  });
+  head.isEnd = true;
+  head.route?.push({
+    verb: v,
+    action: a,
+  });
+};
+
+const matchRoute = (components: string[], verb: string) => {
+  //console.log("======================", components);
+  let head = root;
+  let i = 0;
+  let leafNode: Tree | undefined = undefined;
+  let paramNodes: string[] = [];
+  const traverseTree = (node: Tree, components: string[], i: number) => {
+    if (i >= components.length || !node) return;
+    if (components[i]?.startsWith("?")) return node;
+    let foundNode = TreeHelper.searchByValue(node, components[i]!);
+    //console.log("------------\n", foundNode);
+    if (!foundNode && node.children.length === 1) {
+      if (node.children[0]?.type === "param") {
+        paramNodes.push(
+          `${node.children[0].value.substring(1, node.children[0].value.length - 1)}=${components[i]}`,
+        );
+      }
+
+      if (i < components.length - 1) {
+        foundNode = node.children[0]!;
+      } else {
+        if (node.children[0]?.isEnd === true) {
+          return node.children[0];
+        }
+      }
+    }
+
+    if (!foundNode) return;
+    // if (foundNode && foundNode.isEnd && i === components.length - 1) {
+    //   return foundNode;
+    // }
+
+    return traverseTree(foundNode, components, i + 1);
+  };
+
+  leafNode = traverseTree(head, components, 0);
+  paramNodes.sort();
+  //console.log(leafNode);
+  if (leafNode) {
+    let action = "";
+    leafNode.route?.forEach((r) => {
+      if (r.verb == verb) {
+        action = r.action;
+      }
+    });
+
+    if (paramNodes.length > 0) console.log(`${action} ${paramNodes.join(" ")}`);
+    else {
+      console.log(`${action}`);
     }
   } else {
     console.log("404");
   }
-  // console.log(attr);
 };
 
-let mode = 0;
 while (i < data.length) {
   if (data[i] === "") {
-    mode++;
+    mode = 1;
   } else {
     switch (mode) {
       case 0: {
         const [verb, path, action] = data[i]?.split(" ")!;
-        AddPathToTrie(path!, action!, verb!);
+        const pathComponents = parseRequest(path!);
+        addToTree(pathComponents, verb!, action!);
+        //console.log(pathComponents);
         break;
       }
       case 1: {
         const [verb, path] = data[i]?.split(" ")!;
-        HandleRequest(path!, verb!);
+        const pathComponents = parseRequest(path!);
+        //console.log(pathComponents);
+        matchRoute(pathComponents, verb!);
         break;
       }
     }
   }
   i++;
 }
-let d = root;
 
-const test = (d: Trie) => {
-  let f = 0;
-  for (let i = 0; i < 27; i++) {
-    if (d.children[i] !== undefined) {
-      console.log(String.fromCharCode(i + 97), d.children[i]);
-      test(d.children[i]!);
-      f = 1;
-    }
-  }
-
-  if (f == 0) return;
+const visualizeTree = (root: Tree) => {
+  root.children.forEach((child) => {
+    console.log(
+      child.type,
+      child.value,
+      child.isEnd,
+      child.children.length,
+      child.route,
+    );
+    visualizeTree(child);
+  });
 };
 
-for (let i = 0; i < 27; i++) {
-  if (d.children[i] !== undefined) {
-    console.log(String.fromCharCode(i + 97));
-    test(d.children[i]!);
-  }
-}
-
-// console.log(root);
+//visualizeTree(root);
